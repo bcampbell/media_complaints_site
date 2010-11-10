dd0032e4e268d669a6ccad57bc075fbe53df12fb
Ben Campbell <ben@scumways.com>
2010-11-10 17:01:17 +0000

    use celeb cases as examples on front page

--- cases/views.py
+++ cases/views.py
@@ -60,6 +60,8 @@ def front_page(request):
     context['top_tags'] = Tag.objects.all().annotate( num_cases=Count('case') ).order_by('-num_cases')[:10]
     context['top_issues'] = Clause.objects.all().annotate( num_cases=Count('case') ).order_by('-num_cases')
 
+    celeb = Tag.objects.get( name='Celebrity' )
+    context['notable_case_list'] = Case.objects.filter( tags__in=[celeb] )
 
     return render_to_response('front_page.html', context)
 
