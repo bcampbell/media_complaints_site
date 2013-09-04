78c1a5e9c58e720b521e4e74eacbbb63d3f5bd14
Ben Campbell <ben@scumways.com>
2013-09-04 06:19:34 +0100

    fiddling with wsgi config

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os, sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..') 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '') 
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
